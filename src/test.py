dd00cf6725e1092d59ffd5a6b996d5d7bef8ad8c
bygzx <hbf303779851@126.com>
2018-03-28 17:20:55 +0800

    init test

--- src/test.py
+++ src/test.py
@@ -1,4 +1,17 @@
-print ("asdasd");
-print ("asdasd");
-print ("asdasd");
-print ("asdasd");
+import pandas as pd
+import pandas_datareader as pds
+# Package and modules for importing data; this code may change depending on pandas version
+#import pandas.io.data as web
+import pandas_datareader.data as web
+import datetime
+import tushare as ts
+import redis
+import time
+conn = redis.Redis('18.219.210.244',password='yangguang', port=6379)  # 本机安装，ip为本地地址
+#d = ts.get_tick_data('601318',date='2017-06-26')
+#print (d);
+conn.set('secret', 'test_1')
+print(conn.get('secret'))
+#ts.get_k_data()
+#e = ts.get_hist_data('601318',start='2017-06-23',end='2017-06-26')
+#print (e);
